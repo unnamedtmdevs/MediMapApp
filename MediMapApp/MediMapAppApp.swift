fb5805a067403141cc80310a23b0435c1b9dfa99
ludik <bakhanetssimon@gmail.com>
2025-12-17 19:09:18 +0300

    Initial Commit

---
+++ MediMapApp/MediMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediMapAppApp.swift
+//  MediMapApp
+//
+//  Created by Simon Bakhanets on 17.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MediMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediMapAppTests/MediMapAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MediMapAppTests.swift
+//  MediMapAppTests
+//
+//  Created by Simon Bakhanets on 17.12.2025.
+//
+
+import Testing
+
+struct MediMapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
